f1c70a9b6494b323347de4be3b2bccc00818e773
Israel Sol <isrucrasus10@gmail.com>
2025-06-21 11:01:35 +0300

    space

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
             <div className="flex flex-col items-center lg:items-start">
-                < motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Yohanan</motion.h1>
+                < motion.h1 variants={container(0)} initial="hidden" animate="visible" className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Johanan</motion.h1>
                 <motion.span  variants={container(0.5)} initial="hidden" animate="visible" className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Fullstack developer</motion.span>
                 <motion.p variants={container(1)} initial="hidden" animate="visible" className='my-2 max-w-xl py-6  tracking-tighter text-lg font-semibold font-sans '>{HERO_CONTENT}</motion.p>
             </div>
